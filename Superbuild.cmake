53ecebf93b564265738b64e7a91f16b8ff9ac047
Russ Taylor <russ@reliasolve.com>
2020-02-14 17:50:02 -0500

    Making recursive SuperBuilds work and having them all send to the same INSTALL locations.

--- Superbuild.cmake
+++ Superbuild.cmake
@@ -1,24 +1,39 @@
-# Arguments that we want our external dependencies to build
-# with.  We need to set the install prefix and paths, but others
-# are optional as well if we want to control the build of all
-# external projects.
+# Arguments that we want all of our external dependencies to build
+# with.
 set(cmake_common_args
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
     -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
     -DCMAKE_CXX_COMPILER:PATH=${CMAKE_CXX_COMPILER}
     -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+    -DBUILD_TESTS:BOOL=${BUILD_TESTS}
+    -DUSE_DOXYGEN:BOOL=${USE_DOXYGEN}
+    -DBUILD_STATIC_LIB:BOOL=${BUILD_STATIC_LIB}
+    -DBUILD_DEB_PACKAGE:BOOL=${BUILD_DEB_PACKAGE}
+)
+
+# The most-external SuperBuild from all projects sets
+# the install directories.  All other internal superbuilds
+# must respect that so that they all use the same one.
+if (SUPERBUILD_INSTALL_SET)
+  set(cmake_common_args ${cmake_common_args}
+    -DLIB_SUFFIX:STRING=${LIB_SUFFIX}
+    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
+    -DCMAKE_INSTALL_RPATH:PATH=${CMAKE_INSTALL_RPATH}
+    -DCMAKE_INSTALL_LIBDIR:PATH=${CMAKE_INSTALL_LIBDIR}
+    -DCMAKE_INCLUDE_PATH:PATH=${CMAKE_INCLUDE_PATH}
+  )
+else ()
+  set(cmake_common_args ${cmake_common_args}
     -DLIB_SUFFIX:STRING=${LIB_SUFFIX}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/INSTALL
     -DCMAKE_INSTALL_RPATH:PATH=${CMAKE_BINARY_DIR}/INSTALL/lib${LIB_SUFFIX}
     -DCMAKE_INSTALL_LIBDIR:PATH=lib
     -DCMAKE_INCLUDE_PATH:PATH=${CMAKE_BINARY_DIR}/INSTALL/include
-    -DBUILD_TESTS:BOOL=${BUILD_TESTS}
-    -DUSE_DOXYGEN:BOOL=${USE_DOXYGEN}
-    -DBUILD_STATIC_LIB:BOOL=${BUILD_STATIC_LIB}
-    -DBUILD_DEB_PACKAGE:BOOL=${BUILD_DEB_PACKAGE}
-    -DUSE_SUPERBUILD:BOOL=OFF
-)
+    -DSUPERBUILD_INSTALL_SET:BOOL=ON
+  )
+endif ()
+
 
 add_custom_target(submodule_init
     COMMAND ${GIT_EXECUTABLE} submodule init ${CMAKE_SOURCE_DIR}
@@ -45,12 +60,19 @@ if (BUILD_TESTS)
   set (acl_depends JsonBox)
 endif (BUILD_TESTS)
 
+# When we build ourself recursively, we turn off USE_SUPERBUILD.
 ExternalProject_Add(acl
   SOURCE_DIR ${CMAKE_SOURCE_DIR}
   BUILD_ALWAYS 1
   CMAKE_ARGS ${cmake_common_args} 
+    -DUSE_SUPERBUILD:BOOL=OFF
   INSTALL_DIR ${CMAKE_BINARY_DIR}/INSTALL
   LOG_INSTALL 1
   DEPENDS ${acl_depends}
 )
+
+# If someone is making install on the SuperBuild, it is probably another
+# nested Superbuild.  In that case, we've already done all we need to do.
+
+install(CODE "message(\"Install already done by Superbuild.\")")
 
