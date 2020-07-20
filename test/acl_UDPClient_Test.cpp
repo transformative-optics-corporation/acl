46a32498b186640ebffb80a91a6c1478222fd713
kamatsumeet <choomeet@gmail.com>
2020-07-20 11:45:31 -0400

    Minor changes specific to requirement in UDP client test program

--- test/acl_UDPClient_Test.cpp
+++ test/acl_UDPClient_Test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/time.h>
 #include <CoreSocket.hpp>
 
 using namespace acl::CoreSocket;
@@ -7,27 +8,40 @@ int main()
 {
   const char* ip_address = "10.0.0.89";
   unsigned short port_number = 8090;
-  char write_buffer[6] = {0};
+  char write_buffer[6] = {0x00,0x01,0x00,0x00,0x00,0x00};
   char read_buffer[10] = {0};
+  struct timeval timeout;
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
 
   SOCKET s = connect_udp_port(ip_address, port_number);
   if (s == BAD_SOCKET){
     std::cerr << "Failed to open UDP socket!" << std::endl;
     return -1;
+  }else{
+    std::cerr <<"Connected via UDP!" << std::endl;
   }
 
   if (6 != noint_block_write(s, write_buffer, 6)) {
     std::cerr <<"Error while writing!" << std::endl;
     return -2;
+  }else{
+    std::cerr <<"Write Successful!" << std::endl;
   }
 
-  if (10 != noint_block_read(s, read_buffer, 10)) {
-    std::cerr <<"Error while reading!" << std::endl;
+  int n = noint_block_read_timeout(s, read_buffer, 10, &timeout);
+  if (n<=0) {
+    std::cerr <<"Error while reading! " << n << std::endl;
     return -3;
+  }else{
+    std::cerr <<"Read Successful!" << std::endl;
   }
+
+  for(int i=0;i<n;++i)
+    printf("%02X\n", (unsigned char)read_buffer[i]);
 
   close_socket(s);
-  std::cout << "Success!" << std::endl;
+  std::cout << "Success closing the socket!" << std::endl;
   return 0;
 }
 
