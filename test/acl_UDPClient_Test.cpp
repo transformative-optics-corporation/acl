bc54e71ed70e9e5a13309f85b42d7f7448b9493c
Russ Taylor <russ@reliasolve.com>
2020-07-20 09:26:45 -0400

    New version of UDP test program that fixes a few bugs.

--- test/acl_UDPClient_Test.cpp
+++ test/acl_UDPClient_Test.cpp
@@ -3,82 +3,31 @@
 
 using namespace acl::CoreSocket;
 
-SOCKET g_socket;
-unsigned short port_number = 8090;
-const char* ip_address = "10.0.0.89";
-uint8_t write_buffer[6] = {0x00,0x01,0x00,0x00,0x00,0x00};
-uint8_t read_buffer[10] = {0};
+int main()
+{
+  const char* ip_address = "10.0.0.89";
+  unsigned short port_number = 8090;
+  char write_buffer[6] = {0};
+  char read_buffer[10] = {0};
 
-void connect_camera(unsigned short* portno, const char* IPaddress){
-
-    g_socket = open_udp_socket(portno, IPaddress, true);
-    //g_socket = connect_udp_port(IPaddress, 8090);
-	if(g_socket == BAD_SOCKET){
-		std::cout << "Failed to open UDP socket ! Exiting !" << std::endl;
-		exit(0);
-	}
-}
-
-void disconnect_camera(){
-
-    close_socket(g_socket);
-}
-
-int write_data(uint8_t* buffer, int bytes, SOCKET s, int chunkSize){
-
-   int sofar = 0;
-   int remaining = bytes;
-
-   // Send all the bytes 
-   while (remaining > 0) {
-     int nextChunk = chunkSize;
-     if (nextChunk > remaining) {
-       nextChunk = remaining;
-     }
-
-     printf("Writing: %X\n",(unsigned char)buffer[sofar]);
-     if (nextChunk != noint_block_write(s, (const char*)buffer[sofar], nextChunk)) {
-        std::cout <<"Error while writing !" << std::endl;
-       return -1;
-     }
-     sofar += nextChunk;
-     remaining -= nextChunk;
-   }
-
-   return 0;
- }
-
-int read_data(uint8_t* buffer, int bytes, SOCKET s, int chunkSize){
-
-  int sofar = 0;
-  int remaining = bytes;
-
-  // Get all the bytes
-  while (remaining > 0) {
-    int nextChunk = chunkSize;
-    if (nextChunk > remaining) {
-      nextChunk = remaining;
-    }
-
-    if (nextChunk != noint_block_read(s, (char*)buffer[sofar], nextChunk)) {
-        std::cout <<"Error while reading !" << std::endl;
-      return -1;
-    }
-    sofar += nextChunk;
-    remaining -= nextChunk;
+  SOCKET s = connect_udp_port(ip_address, port_number);
+  if (s == BAD_SOCKET){
+    std::cerr << "Failed to open UDP socket!" << std::endl;
+    return -1;
   }
 
+  if (6 != noint_block_write(s, write_buffer, 6)) {
+    std::cerr <<"Error while writing!" << std::endl;
+    return -2;
+  }
+
+  if (10 != noint_block_read(s, read_buffer, 10)) {
+    std::cerr <<"Error while reading!" << std::endl;
+    return -3;
+  }
+
+  close_socket(s);
+  std::cout << "Success!" << std::endl;
   return 0;
 }
 
-int main(){
-
-	connect_camera(&port_number, ip_address);
-
-	write_data(write_buffer, 6, g_socket, 1);
-
-	read_data(read_buffer, 10, g_socket, 1);
-
-	disconnect_camera();
-
-}	
